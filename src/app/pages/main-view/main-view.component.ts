423b8f0181c585ae88092dcff29a5634629a5fb6
Patryk Brilla <patryk.brilla@gmail.com>
2021-10-29 13:02:11 +0200

    29/10 Simple Kanban - During

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.scss']
+  styleUrls: ['./main-view.component.scss'],
 })
-export class MainViewComponent implements OnInit {
+export class MainViewComponent {
+  constructor() {}
 
-  constructor() { }
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      'Some random idea',
+      'Some random idea',
+      'Some random idea',
+    ]),
+    new Column('Tasks', [
+      'Some random task 1',
+      'Some random task 2',
+      'Some random task 3',
+      'Some random task 4',
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep',
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog',
+    ]),
+  ]);
 
-  ngOnInit(): void {
+  dropColumn(event: CdkDragDrop<Column[]>) {
+    moveItemInArray(
+      this.board.columns,
+      event.previousIndex,
+      event.currentIndex
+    );
   }
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addTask(columnData: string, column: Column) {
+    column.tasks.push(columnData);
+  }
 }
